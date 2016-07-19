d306ffe362321d54e9547d6db738fb87dd378f95
emilieg <emilie.gerber@hotmail.com>
2016-07-19 14:26:25 -0700

    post route and request to post form items returning status 200

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/public'));
-
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 
 });
+
+app.post('/', function(req,res) {
+  res.send('posting');
+  console.log('posting')
+})
+//using the request.post method 
+request.post({url:'http://localhost:3000',
+  form: {
+    name: 'Emilie',
+    email: 'emilie@gmail.com',
+    password: 'summertime'
+  }}, function(err, res, body){
+    if(err) {
+      console.log("there was an ERROR: ", err);
+    } else {
+      console.log(res.statusCode);
+    }
+  }
+);
+
+
 
 
 app.listen(3000);
