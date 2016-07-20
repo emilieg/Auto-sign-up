665391f071d3b77f50905bd8a76280979feb53ba
emilieg <emilie.gerber@hotmail.com>
2016-07-20 11:50:57 -0700

    findOrCreated works with one user entry from request.post

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
-var session = require('session');
+var session = require('express-session');
+var db = require('./models');
+
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -22,15 +24,28 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req,res) {
-  res.send('posting');
-  console.log(req.body);
-})
+  db.user.findOrCreate({
+    where: {
+    name: req.body.name
+  },
+    defaults: {
+      email: req.body.email,
+      password: req.body.password
+    }
+  }).spread(function(user, created) {
+    console.log("created");
+  }).catch(function(err) {
+    res.send(err)
+  });
+  // res.send('posting');
+  // console.log(req.body);
+});
 //using the request.post method 
 request.post({url:'http://localhost:3000',
   form: {
-    name: ['Emilie','Kathy', 'Tonya'],
-    email: ['emilie@gmail.com','kathy@email.com', 'tonya@gmail.com'],
-    password: ['summertime', 'lazy', 'teapot']
+    name: 'Emilie',
+    email: 'emilie@gmail.com',
+    password: 'summertime',
   }}, function(err, res, body){
     if(err) {
       console.log("there was an ERROR: ", err);
---
+++ models/user.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }},
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
