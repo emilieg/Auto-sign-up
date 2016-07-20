b1ec3e00ec6b24d99954236479e3927533761df9
emilieg <emilie.gerber@hotmail.com>
2016-07-20 10:38:57 -0700

    logging req.body

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+
+
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 
@@ -12,14 +17,14 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req,res) {
   res.send('posting');
-  console.log('posting')
+  console.log(req.body);
 })
 //using the request.post method 
 request.post({url:'http://localhost:3000',
   form: {
-    name: 'Emilie',
-    email: 'emilie@gmail.com',
-    password: 'summertime'
+    name: ['Emilie','Kathy', 'Tonya'],
+    email: ['emilie@gmail.com','kathy@email.com', 'tonya@gmail.com'],
+    password: ['summertime', 'lazy', 'teapot']
   }}, function(err, res, body){
     if(err) {
       console.log("there was an ERROR: ", err);
